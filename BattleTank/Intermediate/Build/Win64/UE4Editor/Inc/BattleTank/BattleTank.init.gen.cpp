5abbdd95e25c893db0e46d0fc5ac63cf9a1cbfb7
DanPaddock <danpaddk@gmail.com>
2018-09-02 17:39:28 -0400

    Added throttles to move tank, not implemented

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BattleTank",
 				PKG_CompiledIn | 0x00000000,
-				0x53381492,
-				0x9579269D,
+				0x2C8E7529,
+				0x9E585F14,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
